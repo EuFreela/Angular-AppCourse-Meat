d9ca9723b72094b6ed149189fa7f9dff730fa212
Freelancer <lameck.freelas@gmail.com>
2018-03-02 12:20:18 -0300

    Implementação do carrinho de compras. Adicionar e remover itens

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,6 @@
+import { ShoppingCartService } from './shopping-cart.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'mt-shopping-cart',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit() {
   }
+
+  items(): any[]  {
+    return this.shoppingCartService.items;
+  }
+
+  total(): number{
+    return this.shoppingCartService.total()
+  }
+
+  clear(){
+    this.shoppingCartService.clear()
+  }
+
+  removeItem(item: any){
+    this.shoppingCartService.removeitem(item)
+  }
+
+  addItem(item: any){
+    this.shoppingCartService.addItem(item)
+  }
+
 
 }
